9627aca4df1cfb6b723bed7dd575f22d82c98e19
hyuk9 <107982323+hyuk9@users.noreply.github.com>
2022-12-13 11:18:35 +0900

    오류수정

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 유효성 체크 라이브러리 : vee-validate ( Vue 전용 )
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate);
+
 Vue.config.productionTip = false
 
 new Vue({
