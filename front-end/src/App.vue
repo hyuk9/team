9d4be2d67cfe55c1b8cc2df21d24858aab2f1642
hyuk9 <107982323+hyuk9@users.noreply.github.com>
2022-12-14 11:36:56 +0900

    재업로드

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,11 +3,6 @@
     <NavCom v-if="wantToShow()" />
     <router-view />
     <FooterCom v-if="wantToShow()" />
-    <!-- <div id="app">
-      <NavCom />
-      <router-view />
-      <FooterCom />
-    </div> -->
   </div>
 </template>
 
@@ -15,7 +10,26 @@
 <script>
 import NavCom from "@/components/common/NavCom.vue";
 import FooterCom from "@/components/common/FooterCom.vue";
+import Darkmode from "darkmode-js";
+
 export default {
+  data() {
+    return {
+      options: {
+        bottom: "64px", // default: '32px'
+        right: "unset", // default: '32px'
+        left: "32px", // default: 'unset'
+        time: "0.5s", // default: '0.3s'
+        mixColor: "#fff", // default: '#fff'
+        backgroundColor: "#fff", // default: '#fff'
+        buttonColorDark: "#100f2c", // default: '#100f2c'
+        buttonColorLight: "#fff", // default: '#fff'
+        saveInCookies: false, // default: true,
+        label: "🌓", // default: ''
+        autoMatchOsTheme: true, // default: true
+      },
+    };
+  },
   methods: {
     wantToShow() {
       if (this.$route.name == "login") {
@@ -29,18 +43,45 @@ export default {
     NavCom,
     FooterCom,
   },
+  mounted() {
+    const darkmode = new Darkmode(this.options);
+    darkmode.showWidget();
+  },
 };
 </script>
 
 <style lang="scss">
-h1, span {
+h1,
+h5,
+p,
+span,
+#menu,
+.login,
+.register,
+.profile,
+.logout,
+#navbarNavDropdown {
   font-family: ONE-Mobile-POP;
 }
 
 @font-face {
-  font-family: 'ONE-Mobile-POP';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff') format('woff');
-  font-weight: normal;
+  font-family: "ONE-Mobile-POP";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff")
+    format("woff");
+  font-weight: lighter;
   font-style: normal;
 }
+
+
+// #typed .typed-words {
+//       position: relative; }
+//       #typed .typed-words:before {
+//         position: absolute;
+//         height: 5px;
+//         background-color: #ffd75f;
+//         bottom: 0;
+//         left: 0;
+//         right: 0;
+//         content: ""; 
+//         }
 </style>
