a9ad917c90d9d0335b02f5df44b1b2c0eef03dc6
eodalzh <jsh9239@hanmail.net>
2022-12-22 13:03:50 +0900

    2022-12-22 프로필 페이지

--- front-end/src/components/reservation/AddReservation.vue
+++ front-end/src/components/reservation/AddReservation.vue
@@ -131,10 +131,99 @@
         </div>
       </div>
     </section>
+    <!-- datepicker 시작 -->
+    <div class="container-fluid px-0 px-sm-4 mx-auto">
+      <div class="row justify-content-center mx-0">
+        <div class="col-lg-10">
+          <div class="card border-0">
+            <form autocomplete="off">
+              <div class="card-header bg-dark">
+                <div
+                  class="mx-0 mb-0 row justify-content-sm-center justify-content-start px-1"
+                >
+                  <input
+                    type="text"
+                    id="dp1"
+                    class="datepicker"
+                    placeholder="Pick Date"
+                    name="date"
+                    readonly
+                  /><span class="fa fa-calendar"></span>
+                </div>
+              </div>
+              <div class="card-body p-3 p-sm-5">
+                <div class="row text-center mx-0">
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">11:00AM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">11:30AM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">12:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">12:30PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">13:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">13:30PM</div>
+                  </div>
+                </div>
+                <div class="row text-center mx-0">
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">14:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">14:30PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">15:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">15:30PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">16:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">16:30PM</div>
+                  </div>
+                </div>
+                <div class="row text-center mx-0">
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">17:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">17:30PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">18:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">18:30PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">19:00PM</div>
+                  </div>
+                  <div class="col-md-2 col-4 my-1 px-2">
+                    <div class="cell py-1">19:30PM</div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- datepicker 끝 -->
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import ReservationDataService from "../../services/ReservationDataService";
 
 export default {
@@ -143,7 +232,7 @@ export default {
       reservation: {
         rid: null,
         restaurant: "", // 가게명
-        rname: "", // 예약자명 
+        rname: "", // 예약자명
         rcount: "", // 인원수
         phone: "", // 연락처
         reservationDate: "", // 예약날짜
@@ -191,8 +280,169 @@ export default {
       this.reservation = {};
     },
   },
-  mounted() {},
+  mounted() {
+    $(document).ready(function () {
+      $(".datepicker").datepicker({
+        format: "dd-mm-yyyy",
+        autoclose: true,
+        startDate: "0d",
+      });
+
+      $(".cell").click(function () {
+        $(".cell").removeClass("select");
+        $(this).addClass("select");
+      });
+    });
+  },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  color: #000;
+  overflow-x: hidden;
+  height: 100%;
+  background-color: #c5cae9 !important;
+  background-repeat: no-repeat;
+  padding: 0px !important;
+}
+
+.container-fluid {
+  padding-top: 120px !important;
+  padding-bottom: 120px !important;
+}
+
+.card {
+  box-shadow: 0px 4px 8px 0px #7986cb;
+}
+
+input {
+  padding: 10px 20px !important;
+  border: 1px solid #000 !important;
+  border-radius: 10px;
+  box-sizing: border-box;
+  background-color: #616161 !important;
+  color: #fff !important;
+  font-size: 16px;
+  letter-spacing: 1px;
+  width: 180px;
+}
+
+input:focus {
+  -moz-box-shadow: none !important;
+  -webkit-box-shadow: none !important;
+  box-shadow: none !important;
+  border: 1px solid #512da8;
+  outline-width: 0;
+}
+
+::placeholder {
+  color: #fff;
+  opacity: 1;
+}
+
+:-ms-input-placeholder {
+  color: #fff;
+}
+
+::-ms-input-placeholder {
+  color: #fff;
+}
+
+button:focus {
+  -moz-box-shadow: none !important;
+  -webkit-box-shadow: none !important;
+  box-shadow: none !important;
+  outline-width: 0;
+}
+
+.datepicker {
+  background-color: #000 !important;
+  color: #fff !important;
+  border: none;
+  padding: 10px !important;
+}
+
+.datepicker-dropdown:after {
+  border-bottom: 6px solid #000;
+}
+
+thead tr:nth-child(3) th {
+  color: #fff !important;
+  font-weight: bold;
+  padding-top: 20px;
+  padding-bottom: 10px;
+}
+
+.dow,
+.old-day,
+.day,
+.new-day {
+  width: 40px !important;
+  height: 40px !important;
+  border-radius: 0px !important;
+}
+
+.old-day:hover,
+.day:hover,
+.new-day:hover,
+.month:hover,
+.year:hover,
+.decade:hover,
+.century:hover {
+  border-radius: 6px !important;
+  background-color: #eee;
+  color: #000;
+}
+
+.active {
+  border-radius: 6px !important;
+  background-image: linear-gradient(#90caf9, #64b5f6) !important;
+  color: #000 !important;
+}
+
+.disabled {
+  color: #616161 !important;
+}
+
+.prev,
+.next,
+.datepicker-switch {
+  border-radius: 0 !important;
+  padding: 20px 10px !important;
+  text-transform: uppercase;
+  font-size: 20px;
+  color: #fff !important;
+  opacity: 0.8;
+}
+
+.prev:hover,
+.next:hover,
+.datepicker-switch:hover {
+  background-color: inherit !important;
+  opacity: 1;
+}
+
+.cell {
+  border: 1px solid #bdbdbd;
+  margin: 2px;
+  cursor: pointer;
+}
+
+.cell:hover {
+  border: 1px solid #3d5afe;
+}
+
+.cell.select {
+  background-color: #3d5afe;
+  color: #fff;
+}
+
+.fa-calendar {
+  color: #fff;
+  font-size: 30px;
+  padding-top: 8px;
+  padding-left: 5px;
+  cursor: pointer;
+}
+</style>
