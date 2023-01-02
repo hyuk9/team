571f4e66ed97e27daebb66d393fad017aed54831
Messier-81 <6461372@gmail.com>
2023-01-02 12:57:05 +0900

    12

--- front-end/src/components/board/free/FreeView.vue
+++ front-end/src/components/board/free/FreeView.vue
@@ -47,13 +47,8 @@
                 <div class="row">
                   <div class="col-sm-4" v-for="(data, index) in free" :key="index">
                     <div class="card">
-                      <img :src="data.galleryUrl" class="card-img-top" alt="강의" />
+                      <img :src="data.fileUrl" class="card-img-top" alt="강의" />
                       <div class="card-body">
-                        <h5 class="card-title">{{ data.galleryTitle }}</h5>
-                        <a style="color: inherit" @click="deleteImage(data.fno)">
-                          <!-- <!— <i class="fas fa-trash" /> —> -->
-                          <span class="badge bg-danger">Delete</span>
-                        </a>
                       </div>
                     </div>
                   </div>
@@ -64,23 +59,22 @@
               <th scope="row">이미지 업로드</th>
               <td scope="row">
                 <!-- 이미지명(galleryTitle) 입력박스 시작 -->
-                <div class="mb-3 col-md-5">
+                <!-- <div class="mb-3 col-md-5">
                   <label for="galleryTitle" class="form-label">이미지명</label>
                   <input type="text" class="form-control" id="galleryTitle" required name="galleryTitle"
                     v-model="galleryTitle" />
-                </div>
+                </div> -->
                 <!-- 이미지명 입력박스 끝 -->
 
                 <!-- 이미지 선택상자 시작 -->
-                <div class="mb-3 col-md-5">
+                <!-- <div class="mb-3 col-md-5">
                   <label class="btn btn-default p-0">
-                    <!-- <!— 파일 선택상자 —> -->
                     <input type="file" accept="image/*" ref="file" @change="selectImage" />
                   </label>
                   <button class="btn btn-success btn-sm float-left" :disabled="!currentImage" @click="upload">
                     Upload
                   </button>
-                </div>
+                </div> -->
                 <!-- 이미지 선택상자 끝 -->
                 <!-- upload 버튼 : insert 문 실행 끝 -->
               </td>
@@ -102,7 +96,6 @@
 
 <script>
 import FreeDataService from "@/services/FreeDataService";
-import GalleryDataService from "@/services/GalleryDataService";
 export default {
   data() {
     return {
@@ -116,7 +109,7 @@ export default {
       // springboot 요청할 변수 , 이미지명(galleryTitle), 내용(content)
       galleryTitle: "",
 
-            // 페이징을 위한 변수 정의
+      // 페이징을 위한 변수 정의
       page: 1, // 현재 페이지
       count: 0, // 전체 데이터 건수
       pageSize: 3, // 한페이지당 몇개를 화면에 보여줄지 결정하는 변수
@@ -171,90 +164,66 @@ export default {
           console.log(e);
         });
     },
-    // 전체 조회 함수
-    retrieveGallery() {
-      GalleryDataService.getFiles(this.searchTitle,this.fno, this.page - 1, this.pageSize)
-        // 성공하면 .then() 결과가 전송됨
-        .then((response) => {
-          const { free, totalItems } = response.data; // springboot 의 전송된 맵 정보
-          this.free = free; // 스프링부트에서 전송한 데이터
-          this.count = totalItems; // 스프링부트에서 전송한 페이지정보(총 건수)
-          // 디버깅 콘솔에 정보 출력
-          console.log(response.data);
-        })
-        // 실패하면 .catch() 에 에러가 전송됨
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    // 파일 선택상자에서 선택한 이미지를 저장하는 함수
-    selectImage() {
-      // 첫번째 선택한 이미지를 변수에 저장
-      // this.$refs : $refs 속성이 있는 컨트롤이 선택됨
-      this.currentImage = this.$refs.file.files.item(0);
-      // .createObjectURL() : 이미지 주소만 참조해서 이미지 보여주기 함수
-      this.previewImage = URL.createObjectURL(this.currentImage);
-      this.message = "";
-    },
-    // upload 함수
-    upload() {
-      GalleryDataService.upload(
-        this.galleryTitle,
-        this.currentImage,
-        this.fno
-      )
-        // insert 성공 then()
-        .then((response) => {
-          // 서버쪽 성공 메세지를 저장
-          this.message = response.data.message;
 
-          // 화면에 재조회 요청(axios 함수로 재조회 요청)
-          return GalleryDataService.getFiles(
-            this.searchTitle,
-            this.page - 1,
-            this.pageSize
-          );
-        })
-        // 조회가 성공하면 실행되는 then()
-        .then((response2) => {
-          const { free, totalItems } = response2.data;
+    retrieveFree() {
+      FreeService.getAll(
+        this.searchSelect,
+        this.searchKeyword,
+        this.page - 1,
+        this.pageSize)
+        .then((response) => {
+          const { free, totalItems } = response.data;
           this.free = free;
           this.count = totalItems;
-          console.log(response2.data);
+
+          console.log(response.data);
         })
         .catch((e) => {
           console.log(e);
-          this.message = "Could not upload the image!" + e;
-          this.currentImage = undefined;
+
+          this.message = "조회 시 에러가 발생했습니다. " + e.message;
+          // toast 알림창 호출
+          // 2. 알램창 호출
+          this.$toastr.e("에러", this.message, { timeOut: 1000 });
         });
     },
-    // select box 값 변경시 실행되는 함수(재조회)
-    handlePageSizeChange(event) {
-      this.pageSize = event.target.value; // 한페이지당 개수 저장(3, 6, 9)
-      this.page = 1;
-      // 재조회 함수 호출
-      this.retrieveGallery();
-    },
-    // 페이지 번호 변경시 실행되는 함수(재조회)
-    handlePageChange(value) {
-      this.page = value; // 매개변수값으로 현재페이지 변경
-      // 재조회 함수 호출
-      this.retrieveGallery();
-    },
-    deleteImage(fno) {
-      GalleryDataService.delete(fno)
-        .then(response => {
-          console.log(response);
-          this.message = "정상적으로 삭제되었습니다.";
 
-          // 삭제 후 재조회
-          this.retrieveGallery();
+    // Todo : handlePageChange, handlePageSizeChange
+    handlePageChange(value) {
+      this.page = value;
+      this.retrieveFree();
+    },
+
+    handlePageSizeChange(event) {
+      this.pageSize = event.target.value;
+      this.page = 1;
+      this.retrieveFree();
+    },
+    deleteCourse(id) {
+      // alert(id);
+      FreeService.delete(id)
+        .then((response) => {
+          console.log(response);
+
+          this.message = "정상적으로 삭제되었습니다. ";
+          // toast 알림창 호출
+          this.$toastr.s("성공", this.message, { timeOut: 1000 });
+
+          //  삭제 후 재조회
+          this.retrieveFree();
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e);
-          this.message = " 삭제 시 에러가 발생했습니다." + e.message;
-        })
-    }
+
+          this.message = "삭제 시 에러가 발생했습니다. " + e.message;
+          // toast 알림창 호출
+          // toast 알림창 호출
+          // 1. 알림창 위치
+          this.$toastr.options.positionClass = "toast-top-center";
+          // 2. 알램창 호출
+          this.$toastr.e("에러", this.message, { timeOut: 1000 });
+        });
+    },
   },
 
   computed: {
@@ -281,7 +250,7 @@ export default {
   mounted() {
     this.message2 = "";
     this.getFree(this.$route.params.fno);
-    this.retrieveGallery();
+    this.retrieveFree();
   },
 };
 </script>
