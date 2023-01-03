782a2355e0ac69225e9abde15ab5a781b6a21569
eodalzh <jsh9239@hanmail.net>
2023-01-03 14:38:42 +0900

    2023-01-03 찜하기

---
+++ front-end/src/services/FavoriteDataService.js
@@ -0,0 +1,34 @@
+// FavoriteDataService.js
+import http from "../http-common";
+
+class FavoriteDataService {
+  getAll(searchSelect, searchKeyword, page, size) {
+    // get 방식 통신 요청 -> @GetMapping("/api/faq")
+    return http.get(
+      `/favorite?searchSelect=${searchSelect}&searchKeyword=${searchKeyword}&page=${page}&size=${size}`
+    );
+  }
+
+  get(fid) {
+    return http.get(`/favorite/${fid}`);
+  }
+
+  create(data) {
+    console.log(data);
+    return http.post("/favorite", data);
+  }
+
+  update(fid, data) {
+    return http.put(`/favorite/${fid}`, data);
+  }
+
+  delete(fid) {
+    return http.delete(`/favorite/deletion/${fid}`);
+  }
+
+  deleteAll() {
+    return http.delete("/favorite/all");
+  }
+}
+
+export default new FavoriteDataService();
