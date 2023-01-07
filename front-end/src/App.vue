78b40a5a1ca82030f67ced9325a557f73bc71d96
JoSeongmo <108975002+JoSeongmo@users.noreply.github.com>
2023-01-07 18:07:26 +0900

    Revert "0107 브라우저 꺼지면 자동 로그아웃"
    This reverts commit 2f5534c8bf545d39862f2d62f0da75c77d926b25.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -61,8 +61,6 @@ export default {
   mounted() {
     const darkmode = new Darkmode(this.options);
     darkmode.showWidget();
-    // 브라우저 꺼지면 자동으로 로그아웃하기
-    window.addEventListener('unload',()=> this.$store.dispatch("auth/logout"));
   },
 };
 </script>
