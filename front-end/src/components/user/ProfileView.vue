4c13180d0d2a6161b7cf663cf2e036710227b6b4
eodalzh <jsh9239@hanmail.net>
2023-01-07 12:58:06 +0900

    최근본페이지

--- front-end/src/components/user/ProfileView.vue
+++ front-end/src/components/user/ProfileView.vue
@@ -172,176 +172,7 @@
     <!-- 내가 쓴 리뷰 목록 끝 -->
 
     <!-- 최근 본 가게 시작 -->
-    <div class="pt-5 pb-5">
-      <div class="mx-auto text-center mb-5">
-        <h5 class="fw-bold fs-3 fs-lg-5 lh-sm">최근 본 가게</h5>
-      </div>
-      <div class="container">
-        <div class="owl-carousel testimonial-carousel">
-          <!-- 1 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20221113_271/1668319190296wqBDT_JPEG/IMG_6177_2.JPG"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">강릉현대장칼국수 부산2호점</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>매일 11:00 - 21:00 <br />20:30 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-
-          <!-- 2 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20220330_36/1648623511617Jb1zG_JPEG/1643331771309-0.jpg"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">디퀄리타</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>월-토 11:30 - 22:00 일요일 휴무 <br />20:30 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-
-          <!-- 3 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20221109_49/1667988084702HrJOG_JPEG/KakaoTalk_20221020_113119374.jpg"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">후라토식당 서면점</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>매일 11:30 - 22:00 <br />21:00 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-
-          <!-- 4 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20221124_186/1669262891000tEB1Q_PNG/%B7%CE%B0%ED7.png"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">컵플</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>매일 10:00 - 18:30 <br />18:00 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-
-          <!-- 5 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20221008_173/1665158490202Aosi6_JPEG/1AC19864-A1DA-47CC-AE0C-F9FDC8BB1909.jpeg"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">차선책</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>매일 11:00 - 22:00 <br />21:30 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-
-          <!-- 6 -->
-          <div class="position-relative">
-            <div class="card" style="width: 18rem">
-              <img
-                src="https://ldb-phinf.pstatic.net/20220728_8/1659004494387VT8qo_JPEG/E41FD257-DAE2-419C-8F90-85DB4437DC5F.jpeg"
-                class="card-img-top"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title">어어마무시</h5>
-                <div class="card-text">
-                  <span class="text-warning me-2"
-                    ><i class="fas fa-map-marker-alt"></i></span
-                  ><span class="text-primary">서면</span>
-                </div>
-              </div>
-              <ul>
-                <li>매일 12:00 - 21:00 <br />20:00 라스트오더</li>
-              </ul>
-              <div class="card-body btn btn-primary">
-                <router-link to="" class="card-link text-white"
-                  >예약하기</router-link
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <LastviewCom />
     <!-- 최근 본 가게 끝 -->
   </div>
 </template>
@@ -352,6 +183,7 @@ import ProfileDataService from "@/services/ProfileDataService";
 import FavoriteCom from "@/components/user/ProfileCom/FavoriteCom.vue";
 import ReservationCom from "@/components/user/ProfileCom/ReservationCom.vue";
 import ReviewCom from "@/components/user/ProfileCom/ReviewCom.vue";
+import LastviewCom from "@/components/user/ProfileCom/LastviewCom.vue";
 
 export default {
   data() {
@@ -439,6 +271,7 @@ export default {
     FavoriteCom,
     ReservationCom,
     ReviewCom,
+    LastviewCom,
   },
   // 화면이 뜨자마자 실행되는 이벤트
   mounted() {
---
+++ front-end/src/services/LastviewDataService.js
@@ -0,0 +1,47 @@
+// DeptDataService.js
+// 목적 : Dept 정보를 axios 로 CRUD 를 하는 공통 함수들 정의
+import http from "../http-common";
+import authHeader from "@/services/auth/auth-header";
+
+class LastviewDataService {
+  // 모든 부서정보 조회 요청 함수
+  getAll() {
+    // get 방식 통신 요청 👉 @GetMapping("/api/dept")
+    return http.get("/lastview");
+  }
+
+  // 유저id를 받아서 리뷰 조회하는 함수
+  getId(id, page, size) {
+    return http.get(`/lastview?id=${id}&page=${page}&size=${size}`, {
+      headers: authHeader(),
+    });
+  }
+
+  // 음식점번호로 조회 요청 함수
+  get(dno) {
+    // get 방식 통신 요청 👉 @GetMapping("/api/menu/{dno}")
+    return http.get(`/lastview/${dno}`);
+  }
+
+  // 부서정보 생성(insert) 요청 함수
+  // post 방식 통신 요청 -> @PostMapping("/api/diner"), @RequestBody
+  create(data) {
+    console.log(data);
+    return http.post("/lastview", data);
+  }
+
+  // 부서정보 수정(update) 요청 함수
+  // put 방식 통신 요청 -> @PutMapping("/api/diner/{dno}"), @RequestBody
+  update(rno, data) {
+    return http.put(`/lastview/${rno}`, data);
+  }
+
+  // 부서정보 삭제(delete) 요청 함수
+  // delete 방식 통신 요청 -> @DeleteMapping("/api/diner/deletion/{dno}")
+  //                        , @PathVariable
+  delete(rno) {
+    return http.delete(`/lastview/deletion/${rno}`);
+  }
+}
+
+export default new LastviewDataService();
